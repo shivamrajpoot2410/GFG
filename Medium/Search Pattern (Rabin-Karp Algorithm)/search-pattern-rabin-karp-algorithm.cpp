3c849afffce8e9246f6499cceec7346d5ff4f282
shivamrajpoot2410 <112850659+shivamrajpoot2410@users.noreply.github.com>
2024-03-06 00:04:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pattern, string text)
+        {
+            //code here.
+            vector<int>v;
+            int n = text.size();
+            int m = pattern.size();
+            
+            for(int i=0; i<n;i++)
+            {
+                bool match =true;
+                
+                for(int j=0; j<m; j++)
+                {
+                    if(text[i+j] != pattern[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if(match==true) v.push_back(i+1);
+            }
+            return v;
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
